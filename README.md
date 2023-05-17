5d345e4f061f711367add190be60e5c5d37aad22
David Wipperfurth <cobaltbluedw@gmail.com>
2023-05-16 20:43:03 -0500

    fixed minor issues

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# MoodleTeachingAssistantBlock
-Teaching Assistant ChatBot in a Moodle Block
+# Teaching Assistant (Moodle Block)
+
+A Teaching Assistant bot to help learners with course/site content.  Powered by your content and Open AI, the Teaching Assistant can hold intelligible, helpful conversation with learners about your material; answer their questions, help them engage with your product, or study for a test.
+
+
+## Setup
+
+ 1. Install this plugin on your Moodle site.
+ 2. Create an UBE: Oracle Account.
+ 3. Upload your documents to UBE: Oracle
+ 4. Add your Account settings to the global Teaching Assistant Moodle Block.
+ 5. Add the block to desired pages.
--- ube_ta/js/ubeta-bot1.js
+++ ube_ta/js/ubeta-bot1.js
@@ -1,4 +1,26 @@
-console.log('ubeta chatbot script added');
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Block UBE TA
+ *
+ * @package    block_ube_ta
+ * @copyright  2023 David Wipperfurth <dwipperfurth@unboundeducation.org>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 export class UBETA {
 
     //settings
@@ -17,7 +39,6 @@ export class UBETA {
     root;
 
     constructor(settings){
-        console.log('constructing ubeta');
 
         // load settings
         for (let key in settings) {
@@ -40,7 +61,6 @@ export class UBETA {
      * Render the initial HTML for the chat window and connect event listeners
      */
     renderInit(){
-        console.log('initializing ubeta chatbot');
 
         // load css
         const head = document.querySelector("head");
@@ -86,7 +106,6 @@ export class UBETA {
             document.addEventListener("mousemove", this.resizeWindow, false);
         }, false);
         document.addEventListener("mouseup", (e) => {
-            console.log('mouseup');
             document.removeEventListener("mousemove", this.resizeWindow, false);
         }, false);
         
